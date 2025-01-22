f1b6defec3ff70782d92b29ff2eeb085035d0f15
Laksh Santosh Varma <lakshvarma19@gmail.com>
2025-01-23 02:20:35 +0530

    Update settings.py

--- nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
+++ nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
@@ -1,12 +1,7 @@
 import os
 from pymongo import MongoClient
 
-DATABASES = {
-    'default': {
-        'ENGINE': '',  # Set to an empty string or remove this line
-        'NAME': os.getenv('MONGO_DB_NAME', 'nifty50'),
-    }
-}
+DATABASES = {}
 
-mongo_client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
+mongo_client = MongoClient(os.getenv('MONGO_URI', 'mongodb+srv://lakshvarma6:your_password@nifty-mongo.5ztn7.mongodb.net/?retryWrites=true&w=majority&appName=nifty-mongo'))
 mongo_db = mongo_client[os.getenv('MONGO_DB_NAME', 'nifty50')]
