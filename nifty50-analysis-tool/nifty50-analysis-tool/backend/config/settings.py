ddf3448779097aaed9f1a9aafea50cab6ae1e3d1
Laksh Santosh Varma <lakshvarma19@gmail.com>
2025-01-23 01:49:48 +0530

    Update settings.py

--- nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
+++ nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
@@ -1,22 +1,12 @@
 import os
+from pymongo import MongoClient
 
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
+        'ENGINE': '',
         'NAME': os.getenv('MONGO_DB_NAME', 'nifty50'),
-        'CLIENT': {
-            'host': os.getenv('MONGO_URI', 'mongodb://localhost:27017'),
-            'port': 27017,
-            'username': os.getenv('MONGO_USER'),  # For Atlas
-            'password': os.getenv('MONGO_PASS'),  # For Atlas
-            'authSource': 'admin',
-            'authMechanism': 'SCRAM-SHA-1',
-            'ssl': True,  # Required for Atlas
-            'tlsAllowInvalidCertificates': True  # For self-signed certs
-        },
-        'OPTIONS': {
-            'connectTimeoutMS': 30000,  # 30s connection timeout
-            'socketTimeoutMS': 60000,   # 1min query timeout
-        }
     }
 }
+
+mongo_client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
+mongo_db = mongo_client[os.getenv('MONGO_DB_NAME', 'nifty50')]
