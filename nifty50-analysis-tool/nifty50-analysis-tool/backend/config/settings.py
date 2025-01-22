fb9b39f5ab4c100227468638e26c052ea33897f3
Laksh Santosh Varma <lakshvarma19@gmail.com>
2025-01-23 02:38:31 +0530

    Update settings.py

--- nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
+++ nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
@@ -1,7 +1,21 @@
 import os
 from pymongo import MongoClient
 
-DATABASES = {}
+# Django dummy database (required)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+    }
+}
 
+# PyMongo setup for MongoDB
 mongo_client = MongoClient(os.getenv('MONGO_URI', 'mongodb+srv://lakshvarma6:your_password@nifty-mongo.5ztn7.mongodb.net/?retryWrites=true&w=majority&appName=nifty-mongo'))
 mongo_db = mongo_client[os.getenv('MONGO_DB_NAME', 'nifty50')]
+
+# Remove auth/admin/sessions if unused
+INSTALLED_APPS = [
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.sessions',
+    'your_app',
+]
