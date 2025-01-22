6b198ce01fe69867b2ccfba581c324605771d6b5
Laksh Santosh Varma <lakshvarma19@gmail.com>
2025-01-23 01:59:51 +0530

    Update settings.py

--- nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
+++ nifty50-analysis-tool/nifty50-analysis-tool/backend/config/settings.py
@@ -1,13 +1,12 @@
+import os
 from pymongo import MongoClient
 
-class NiftyData:
-    def __init__(self):
-        self.client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
-        self.db = self.client[os.getenv('MONGO_DB_NAME', 'nifty50')]
-        self.collection = self.db['nifty_data']
+DATABASES = {
+    'default': {
+        'ENGINE': '',  # Set to an empty string or remove this line
+        'NAME': os.getenv('MONGO_DB_NAME', 'nifty50'),
+    }
+}
 
-    def insert_data(self, data):
-        self.collection.insert_one(data)
-
-    def get_data(self, query):
-        return self.collection.find(query)
+mongo_client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
+mongo_db = mongo_client[os.getenv('MONGO_DB_NAME', 'nifty50')]
